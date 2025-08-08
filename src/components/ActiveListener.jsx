66bbf5c7e9a1a7aff3d33219d668caa1f439fe76
RamsesCode <RamsesSanchez25@gmail.com>
2025-08-08 10:47:07 -0400

    not my changes but vs code says there changes left to commit

--- src/components/ActiveListener.jsx
+++ src/components/ActiveListener.jsx
@@ -1,25 +1,25 @@
-import React from "react";
-import ListenerCard from "./LIstenerCard";
+// import React from "react";
+// import ListenerCard from "./LIstenerCard";
 
-const Activelistener = ({ user }) => {
-    // console.log("this is user from actiive listener---> ", user)
-    return (
-        <main>
-            <h1>
-                Active Listeners
-            </h1>
-            {/* <div className="active-listener-cards">
-                <ul>
-                    {user.map((element) => (
-                        <li key={user.id}>
-                            <ListenerCard element={element} />
-                        </li>
-                    ))}
-                </ul>
-            </div> */}
+// const Activelistener = ({ user }) => {
+//     // console.log("this is user from actiive listener---> ", user)
+//     return (
+//         <main>
+//             <h1>
+//                 Active Listeners
+//             </h1>
+//             {/* <div className="active-listener-cards">
+//                 <ul>
+//                     {user.map((element) => (
+//                         <li key={user.id}>
+//                             <ListenerCard element={element} />
+//                         </li>
+//                     ))}
+//                 </ul>
+//             </div> */}
 
-        </main>
-    )
-}
+//         </main>
+//     )
+// }
 
-export default Activelistener;
+// export default Activelistener;
