38a856a2d8e18f22e1a4246d9190d62a47a9081d
RamsesCode <RamsesSanchez25@gmail.com>
2025-08-14 10:59:59 -0400

    attempting to fix some things

--- src/components/ListenerCard.jsx
+++ src/components/ListenerCard.jsx
@@ -1,33 +1,41 @@
 import React from "react";
-import logo from "../assets/LocalBeats.png"
-import "./ListenerCard.css"
-
+import logo from "../assets/LocalBeats.png";
+import "./ListenerCard.css";
+import spotifyLogo from "../assets/spotify-logo.png";
 
 const ListenerCard = ({ user, track }) => {
-    // Fallbacks for missing data
-    // const displayName = user?.spotify_display_name || user?.nickname || user?.username || "Unknown";
-    // const songTitle = track?.title || "No song playing";
-    // const songArtist = track?.artist || "";
-    // const songAlbumArt = track?.albumArt || "";
-    // const loc = location || user?.location || "Unknown location";
+  console.log("track data:", track);
+  // Fallbacks for missing data
+  // const displayName = user?.spotify_display_name || user?.nickname || user?.username || "Unknown";
+  // const songTitle = track?.title || "No song playing";
+  // const songArtist = track?.artist || "";
+  // const songAlbumArt = track?.albumArt || "";
+  // const loc = location || user?.location || "Unknown location";
 
-    return (
-        <main className="listener-card-container">
-
-            <div className="listener-card-image">
-                <img className="listener-card-album-art" src={track.album_art} />
-                {/* <img className="profile-image" src={user?.spotify_image || logo} alt="Profile" /> */}
-            </div>
-            <div className="listener-card-info">
-                <p className="listener-card-name">{user.spotify_display_name}</p>
-                <p className="listener-card-song">{track.title}{track.artist ? ` — ${track.artist}` : ""}</p>
-
-                {/* <p className="listener-card-location">{loc}</p> */}
-            </div>
-
-
-        </main>
-    );
-}
+  return (
+    <main className="listener-card-container">
+      <div className="listener-card-image">
+        <img className="listener-card-album-art" src={track.album_art} />
+        {/* <img className="profile-image" src={user?.spotify_image || logo} alt="Profile" /> */}
+      </div>
+      <div className="listener-card-info">
+        <p className="listener-card-name">{user.spotify_display_name}</p>
+        <p className="listener-card-song">{track.title}</p>
+        <p className="listener-card-artist">{track.artist}</p>
+        {/* <p className="listener-card-location">{loc}</p> */}
+      </div>
+      <div className="listener-card-spotify-container">
+        <a
+          href={`https://open.spotify.com/track/${track.spotify_track_id}`}
+          // href={"https://open.spotify.com/track/7FOgcfdz9Nx5V9lCNXdBYv"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="listener-card-spotify" src={spotifyLogo} />
+        </a>
+      </div>
+    </main>
+  );
+};
 
 export default ListenerCard;
