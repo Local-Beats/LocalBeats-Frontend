96e64c0c3359bb7b504a1446d85c81a7e3e5a5ee
rend1027 <rend1027@yahoo.com>
2025-08-19 23:47:07 -0400

    fixed merge issues

--- webpack.config.js
+++ webpack.config.js
@@ -3,82 +3,86 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require("webpack");
 const { GenerateSW } = require('workbox-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-// dotenv removed for Vercel compatibility; use Vercel dashboard for env vars
+require("dotenv").config();
 
-module.exports = (env, argv) => {
-  const isProduction = argv.mode === "production";
+const isProduction = process.env.NODE_ENV === 'production';
+const useHttps = process.env.USE_HTTPS === 'true';
 
-  return {
-    watch: false,
-    mode: argv.mode || "development",
-    entry: "./src/App.jsx",
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "main.js",
-      publicPath: "/", // needed for React Router
-    },
-    devtool: "source-map",
-    plugins: [
-      new webpack.EnvironmentPlugin({
-        API_URL: "http://127.0.0.1:8080",
-        REACT_APP_AUTH0_DOMAIN: "",
-        REACT_APP_AUTH0_CLIENT_ID: "",
-        REACT_APP_AUTH0_AUDIENCE: "",
-        REACT_APP_GOOGLE_MAPS_API_KEY: "",
-      }),
-      new HtmlWebpackPlugin({
-        template: './public/index.html',
-      }),
-      ...(isProduction
-        ? [
-            new GenerateSW({
-              clientsClaim: true,
-              skipWaiting: true,
-              maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // allow files up to 5MB
-            })
-          ]
-        : []),
-      new CopyWebpackPlugin({
-        patterns: [
-          { from: 'public/manifest.json', to: 'manifest.json' },
-          { from: 'public/icons', to: 'icons' },
-          { from: 'public/screenshots', to: 'screenshots' }
-        ]
-      })
+module.exports = {
+  watch: false,
+  mode: isProduction ? "production" : "development",
+  entry: "./src/App.jsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "/", // needed for React Router
+  },
+  devtool: "source-map",
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      API_URL: process.env.API_URL || "http://127.0.0.1:8080",
+      REACT_APP_AUTH0_DOMAIN: process.env.REACT_APP_AUTH0_DOMAIN || "",
+      REACT_APP_AUTH0_CLIENT_ID: process.env.REACT_APP_AUTH0_CLIENT_ID || "",
+      REACT_APP_AUTH0_AUDIENCE: process.env.REACT_APP_AUTH0_AUDIENCE || "",
+      REACT_APP_GOOGLE_MAPS_API_KEY: process.env.REACT_APP_GOOGLE_MAPS_API_KEY || "",
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    ...(isProduction
+      ? [
+        new GenerateSW({
+          clientsClaim: true,
+          skipWaiting: true,
+          maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // allow files up to 5MB
+          cleanupOutdatedCaches: true,
+          navigateFallback: '/index.html',
+          exclude: [/index\.html/],
+        })
+      ]
+      : []),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public/manifest.json', to: 'manifest.json' },
+        { from: 'public/icons', to: 'icons' },
+        { from: 'public/screenshots', to: 'screenshots' }
+      ]
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[hash][ext][query]",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-react"],
-            },
-          },
-        },
-        {
-          test: /\.(png|jpe?g|gif|svg)$/i,
-          type: "asset/resource",
-          generator: {
-            filename: "images/[hash][ext][query]",
-          },
-        },
-        {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"],
-        },
-      ],
-    },
-    resolve: {
-      extensions: [".js", ".jsx"],
-    },
-    devServer: {
-      host: "127.0.0.1",
-      port: 3000,
-      allowedHosts: "all",
-      historyApiFallback: true,
-    },
-  };
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    host: "127.0.0.1",
+    port: 3000,
+    allowedHosts: "all",
+    historyApiFallback: true,
+    server: useHttps ? "https" : "http",
+    hot: true,
+  },
 };
